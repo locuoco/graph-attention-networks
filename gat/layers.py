2a471ad364ca5253ecac07200d594b9c18469f25
Alessandro Lo Cuoco <80710565+locuoco@users.noreply.github.com>
2024-08-24 04:30:40 +0200

    Implemented inductive GAT model, multi-headed GAT layer is now more efficient

--- gat/layers.py
+++ gat/layers.py
@@ -2,108 +2,11 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-class GraphAttention(layers.Layer):
+class MultiHeadGraphAttention(layers.Layer):
 	'''
 		Graph attention mechanism
 		Cfr. P. Velickovic et al. (2017)
 	'''
-	def __init__(
-		self,
-		units,
-		dropout_rate=0,
-		kernel_initializer='glorot_normal',
-		kernel_regularizer=None,
-		**kwargs,
-	):
-		super().__init__(**kwargs)
-		self.units = units
-		self.dropout_rate = dropout_rate
-		self.kernel_initializer = keras.initializers.get(kernel_initializer)
-		self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
-
-	def build(self, input_shape):
-		#  kernel shape = (F, F'),
-		# where F is the number of input features per node and F' is the number of output features per node
-		self.kernel = self.add_weight(
-			shape=(input_shape[0][-1], self.units),
-			trainable=True,
-			initializer=self.kernel_initializer,
-			regularizer=self.kernel_regularizer,
-			name='kernel',
-		)
-		self.bias = self.add_weight(
-			shape=(self.units,),
-			trainable=True,
-			initializer='zeros',
-			name='bias',
-		)
-		self.kernel_attention = self.add_weight(
-			shape=(self.units*2,),
-			trainable=True,
-			initializer=self.kernel_initializer,
-			regularizer=self.kernel_regularizer,
-			name='kernel_attention',
-		)
-		self.built = True
-
-	def call(self, inputs, training):
-		#  node_states shape = (N, F)
-		#  edges shape = (E, 2)
-		# where N is the total number of nodes, F is the number of input features for each node
-		# and E is the total number of graph edges
-		node_states, edges = inputs
-
-		if training:
-			node_states = tf.nn.dropout(node_states, self.dropout_rate)
-
-		# linearly transform node states
-		node_states_transformed = tf.matmul(node_states, self.kernel)
-		# shape = (N, F'), where F' = self.units is the number of output features for each node
-
-		# (1) compute pair-wise attention scores
-		node_states_expanded = tf.gather(node_states_transformed, edges)
-		# shape = (E, 2, F')
-		node_states_expanded = tf.reshape(
-			node_states_expanded, (tf.shape(edges)[0], -1)
-		)
-		# shape = (E, 2F')
-		attention_scores = tf.nn.leaky_relu(
-			tf.tensordot(node_states_expanded, self.kernel_attention, 1)
-		)
-		# shape = (E,)
-
-		# (2) normalize attention scores
-		attention_scores = tf.math.exp(attention_scores)
-		attention_scores_sum = tf.math.unsorted_segment_sum(
-			data=attention_scores,
-			segment_ids=edges[:, 0],
-			num_segments=tf.reduce_max(edges[:, 0])+1,
-		)
-		# shape = (N,)
-		attention_scores_sum = tf.repeat(
-			attention_scores_sum, tf.math.bincount(tf.cast(edges[:, 0], 'int32'))
-		)
-		# shape = (E,)
-		attention_scores_norm = attention_scores / attention_scores_sum
-
-		if training:
-			attention_scores_norm = tf.nn.dropout(attention_scores_norm, self.dropout_rate)
-
-		if training:
-			node_states_transformed = tf.nn.dropout(node_states_transformed, self.dropout_rate)
-
-		# (3) gather node states of neighbors, apply attention scores and aggregate
-		node_states_neighbors = tf.gather(node_states_transformed, edges[:, 1])
-		# shape = (E, F')
-		out = tf.math.unsorted_segment_sum(
-			data=node_states_neighbors*attention_scores_norm[:, tf.newaxis],
-			segment_ids=edges[:, 0],
-			num_segments=tf.shape(node_states)[0],
-		)
-		# shape = (N, F')
-		return tf.nn.bias_add(out, self.bias)
-
-class MultiHeadGraphAttention(layers.Layer):
 	def __init__(
 		self,
 		units,
@@ -116,24 +19,98 @@ class MultiHeadGraphAttention(layers.Layer):
 		**kwargs,
 	):
 		super().__init__(**kwargs)
+		self.units = units
 		self.num_heads = num_heads
 		self.merge_type = merge_type
-		self.attention_layers = [GraphAttention(units, dropout_rate, kernel_initializer, kernel_regularizer) for _ in range(num_heads)]
 		self.activation = activation
+		self.dropout_rate = dropout_rate
+		self.kernel_initializer = keras.initializers.get(kernel_initializer)
+		self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
+		if merge_type == 'concat':
+			self.output_dim = units * num_heads
+		else:
+			self.output_dim = units
+
+	def build(self, input_shape):
+		#  kernel shape = (F, F'),
+		# where F is the number of input features per node and F' is the number of output features per node
+		input_dim = input_shape[0][-1]
+		self.kernel = self.add_weight(
+			shape=(input_dim, self.num_heads*self.units),
+			initializer=self.kernel_initializer,
+			regularizer=self.kernel_regularizer,
+			name='kernel',
+		)
+		self.kernel_attention1 = self.add_weight(
+			shape=(1, self.num_heads, self.units),
+			initializer=self.kernel_initializer,
+			regularizer=self.kernel_regularizer,
+			name='kernel_attention1',
+		)
+		self.kernel_attention2 = self.add_weight(
+			shape=(1, self.num_heads, self.units),
+			initializer=self.kernel_initializer,
+			regularizer=self.kernel_regularizer,
+			name='kernel_attention2',
+		)
+		self.bias = self.add_weight(
+			shape=(self.output_dim,),
+			initializer='zeros',
+			name='bias',
+		)
+		self.built = True
 
 	def call(self, inputs, training):
-		atom_features, pair_indices = inputs
+		x, edges = inputs
+		targets, sources = edges[:, 1], edges[:, 0]
 
-		# obtain outputs from each attention head
-		outputs = [
-			attention_layer([atom_features, pair_indices], training=training)
-			for attention_layer in self.attention_layers
-		]
+		n_nodes = tf.shape(x)[-2]
+
+		#  x shape = (N, F)
+		#  edges shape = (E, 2)
+		# where N is the total number of nodes, F is the number of input features for each node
+		# and E is the total number of graph edges
+
+		if training:
+			x = tf.nn.dropout(x, self.dropout_rate)
+
+		# linearly transform node states
+		x = tf.matmul(x, self.kernel)
+		# shape = (N, H F'), where F' = self.units is the number of output features for each node
+		x = tf.reshape(x, (-1, self.num_heads, self.units))
+		# shape = (N, H, F')
+
+		# (1) compute pair-wise attention scores
+		f_t = tf.reduce_sum(x * self.kernel_attention1, -1)
+		f_t = tf.gather(f_t, targets)
+		f_s = tf.reduce_sum(x * self.kernel_attention2, -1)
+		f_s = tf.gather(f_t, sources)
+		scores = tf.nn.leaky_relu(f_t + f_s)
+		# shape = (E, H, F')
+
+		# (2) normalize attention scores
+		scores = tf.math.exp(scores - tf.gather(tf.math.unsorted_segment_max(scores, targets, n_nodes), targets))
+		# shape = (N,)
+		scores /= tf.gather(tf.math.unsorted_segment_sum(scores, targets, n_nodes) + 1e-7, targets)
+		scores = scores[..., None]
+		# shape = (E,)
+
+		if training:
+			scores = tf.nn.dropout(scores, self.dropout_rate)
+
+		if training:
+			x = tf.nn.dropout(x, self.dropout_rate)
+
+		# (3) gather node states of neighbors, apply attention scores and aggregate
+		out = scores * tf.gather(x, sources)
+		# shape = (E, F')
+		out = tf.math.unsorted_segment_sum(out, targets, n_nodes)
 		# concatenate or average the node states from each head
 		if self.merge_type == 'concat':
-			outputs = tf.concat(outputs, axis=-1)
+			shape = tf.concat((tf.shape(out)[:-2], [self.num_heads * self.units]), axis=0)
+			out = tf.reshape(out, shape)
 		else:
-			outputs = tf.reduce_mean(tf.stack(outputs, axis=-1), axis=-1)
+			out = tf.reduce_mean(out, axis=-2)
 		# activate and return node states
-		return self.activation(outputs)
+		return self.activation(out + self.bias)
 
--- gat/models.py
+++ gat/models.py
@@ -1,8 +1,20 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.utils import Sequence
 
 import gat.layers
+
+class DataGenerator(Sequence):
+	def __init__(self, data, labels):
+		self.data = data
+		self.labels = labels
+
+	def __len__(self):
+		return len(self.data)
+
+	def __getitem__(self, index):
+		return self.data[index], self.labels[index]
 
 class GraphAttentionNetwork(keras.Model):
 	def __init__(
@@ -193,6 +205,60 @@ class GraphAttentionNetworkTransductive2(keras.Model):
 		loss = self.compiled_loss(labels, tf.gather(outputs, indices))
 		# update metric(s)
 		self.compiled_metrics.update_state(labels, tf.gather(outputs, indices))
+
+		return {m.name: m.result() for m in self.metrics}
+
+class GraphAttentionNetworkInductive(keras.Model):
+	def __init__(
+		self,
+		output_dim,
+		**kwargs,
+	):
+		super().__init__(**kwargs)
+		self.attention_layer1 = gat.layers.MultiHeadGraphAttention(256, 4)
+		self.attention_layer2 = gat.layers.MultiHeadGraphAttention(256, 4)
+		self.attention_layer3 = gat.layers.MultiHeadGraphAttention(output_dim, 6, merge_type='avg')
+
+	def call(self, inputs, training):
+		node_states, adj = inputs
+		x = self.attention_layer1([node_states, adj], training=training)
+		x = self.attention_layer2([x, adj], training=training)
+		outputs = self.attention_layer3([x, adj], training=training)
+		return outputs
+
+	def train_step(self, data):
+		graph, labels = data
+
+		with tf.GradientTape() as tape:
+			# forward pass
+			outputs = self(graph, training=True)
+			# compute loss
+			loss = self.compiled_loss(labels, outputs)
+			# add regularization losses
+			loss += tf.reduce_sum(self.losses)
+		# compute gradients
+		grads = tape.gradient(loss, self.trainable_weights)
+		# apply gradients (update weights)
+		self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+		# update metric(s)
+		self.compiled_metrics.update_state(labels, outputs)
+
+		return {m.name: m.result() for m in self.metrics}
+
+	def predict_step(self, graph):
+		# forward pass
+		outputs = self(graph, training=False)
+		# compute probabilities
+		return tf.nn.softmax(outputs)
+
+	def test_step(self, data):
+		graph, labels = data
+		# forward pass
+		outputs = self(graph, training=False)
+		# compute loss
+		loss = self.compiled_loss(labels, outputs)
+		# update metric(s)
+		self.compiled_metrics.update_state(labels, outputs)
 
 		return {m.name: m.result() for m in self.metrics}
 
