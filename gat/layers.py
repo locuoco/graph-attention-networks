Fixed model saving, loading and early stopping; successfully trained inductive model for PPI task with 97.3% micro-averaged F1 score

gat/layers.py
@@ -72,11 +72,13 @@ class MultiHeadGraphAttention(layers.Layer):
 			self.residual_weights = False
 		self.built = True
 
+	@tf.function
 	def call(self, inputs, training):
 		output = self.call_sparse_edges_(inputs, training)
 		# activate and return node states
 		return self.activation(output + self.bias)
 
+	@tf.function
 	def call_sparse_edges_(self, inputs, training):
 		x, edges = inputs
 		targets, sources = edges[:, 1], edges[:, 0]
@@ -101,8 +103,8 @@ class MultiHeadGraphAttention(layers.Layer):
 
 		# (1) compute pair-wise attention scores
 		f_t = tf.reduce_sum(xp * self.kernel_attention1, -1)
-		f_t = tf.gather(f_t, targets)
 		f_s = tf.reduce_sum(xp * self.kernel_attention2, -1)
+		f_t = tf.gather(f_t, targets)
 		f_s = tf.gather(f_s, sources)
 		scores = tf.nn.leaky_relu(f_t + f_s)
 		# shape = (E, H, F')
@@ -112,7 +114,6 @@ class MultiHeadGraphAttention(layers.Layer):
 		# shape = (N,)
 		scores /= tf.gather(tf.math.unsorted_segment_sum(scores, targets, n_nodes), targets)
 		scores = scores[..., None]
-		# shape = (E,)
 
 		if training:
 			scores = tf.nn.dropout(scores, self.dropout_rate)
@@ -133,7 +134,7 @@ class MultiHeadGraphAttention(layers.Layer):
 		# residual (skip) connections
 		if self.residual:
 			if self.residual_weights:
-				out += tf.tensordot(x, self.kernel_residual, axes=1)
+				out = tf.add(out, tf.tensordot(x, self.kernel_residual, axes=1))
 			else:
-				out += x
+				out = tf.add(out, x)
 		return out

gat/models.py
@@ -219,7 +219,7 @@ class GraphAttentionNetworkInductive(keras.Model):
 		self.attention_layer2 = gat.layers.MultiHeadGraphAttention(128, 4, residual=True)
 		self.attention_layer3 = gat.layers.MultiHeadGraphAttention(output_dim, 6, merge_type='avg', residual=True)
 
-	def call(self, inputs, training):
+	def call(self, inputs, training=False):
 		input_features, edges = inputs
 		x = self.attention_layer1([input_features, edges], training=training)
 		x = self.attention_layer2([x, edges], training=training)
@@ -233,9 +233,7 @@ class GraphAttentionNetworkInductive(keras.Model):
 			# forward pass
 			outputs = self(graph, training=True)
 			# compute loss
-			loss = self.compiled_loss(labels, outputs)
-			# add regularization losses
-			loss += tf.reduce_sum(self.losses)
+			loss = self.compiled_loss(labels, outputs, regularization_losses=self.losses)
 		# compute gradients
 		grads = tape.gradient(loss, self.trainable_weights)
 		# apply gradients (update weights)

ppi.py
@@ -6,6 +6,8 @@ import dgl
 
 import gat.models
 
+load_last_weights = True
+
 val_dataset = dgl.data.PPIDataset(mode='valid')
 test_dataset = dgl.data.PPIDataset(mode='test')
 train_dataset = dgl.data.PPIDataset(mode='train')
@@ -21,7 +23,7 @@ mode_labels = [val_labels, test_labels, train_labels]
 for i, dataset in enumerate(mode_datasets):
 	for graph in dataset:
 		# get edges
-		edges = tf.transpose(tf.convert_to_tensor(graph.edges()))
+		edges = tf.transpose(tf.convert_to_tensor(graph.edges(), dtype=tf.int32))
 
 		# get node features
 		features = graph.ndata['feat']
@@ -50,6 +52,7 @@ early_stopping = keras.callbacks.EarlyStopping(
 	monitor='val_f1_score',
 	min_delta=1e-5,
 	patience=100,
+	mode='max',
 	restore_best_weights=True
 )
 
@@ -58,6 +61,12 @@ gat_model = gat.models.GraphAttentionNetworkInductive(output_dim)
 
 # compile model
 gat_model.compile(loss=loss_fn, optimizer=optimizer, metrics=[accuracy_fn, f1_fn])
+
+weightsfile = './weights/ppi.weights.h5'
+
+if load_last_weights and os.path.isfile(weightsfile):
+	gat_model(train_graphs[0]) # force model building
+	gat_model.load_weights(weightsfile)
 
 val_generator = gat.models.DataGenerator(val_graphs, val_labels)
 test_generator = gat.models.DataGenerator(test_graphs, test_labels)
@@ -73,9 +82,9 @@ gat_model.fit(
 
 test_loss, test_accuracy, test_f1 = gat_model.evaluate(test_generator, verbose=0)
 
-gat_model.save('ppi.keras')
+gat_model.save_weights(weightsfile)
 
-print('--'*38 + f'\nTest loss: {test_loss}, accuracy: {test_accuracy*100:.1f}, F1 score: {test_f1*100:.1f}%')
+print('--'*38 + f'\nTest loss: {test_loss:.5f}, accuracy: {test_accuracy*100:.1f}%, F1 score: {test_f1*100:.1f}%')
 
 
 